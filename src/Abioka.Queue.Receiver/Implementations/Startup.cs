0dfd7be51695461ef714482efd32cf9423641c42
Tugrul Elmas <tugrulelmas@Tugruls-MacBook-Pro.local>
2018-11-03 01:56:37 +0300

    added repeater

--- src/Abioka.Queue.Receiver/Implementations/Startup.cs
+++ src/Abioka.Queue.Receiver/Implementations/Startup.cs
@@ -6,15 +6,18 @@ namespace Abioka.Queue.Receiver.Implementations
     internal class Startup : IStartup
     {
         private readonly IEnumerable<IQueueConsumer> consumers;
+        private readonly IRepeater repeater;
 
-        public Startup(IEnumerable<IQueueConsumer> consumers) {
+        public Startup(IEnumerable<IQueueConsumer> consumers, IRepeater repeater) {
             this.consumers = consumers;
+            this.repeater = repeater;
         }
 
         public void Start() {
+            repeater.Repeat(() =>{
             foreach (var consumerItem in consumers) {
                 consumerItem.Consume();
-            }
+            }}, 3);
         }
     }
 }
